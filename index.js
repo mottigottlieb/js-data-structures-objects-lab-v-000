2e8c3bbf2fb7f96d41d12d40c121153a1c99b9ea
Motti Gottlieb <mottigottlieb@gmail.com>
2019-11-27 20:31:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+
+// Follow the steps below, running `learn` as you go to get additional information
+// from the tests. To start, define a `driver` variable and assign it to an
+// `Object`. Various updates will be applied to this variable (destructively and
+// non-destructively) in this lab.
+
+let driver = {};
+
+updateDriverWithKeyAndValue(){
+  
+}
